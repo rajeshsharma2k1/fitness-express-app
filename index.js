736369ba89e08557bbb06bff3f93a56b8e94a73b
Rajesh Sharma <68768920+rajeshsharma2k1@users.noreply.github.com>
2023-08-16 18:15:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const routes = require("./routes/api");
 const db = require("./config/connection");
 const cors = require('cors');
 
-const PORT = 3001;
+const PORT = 3000;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
