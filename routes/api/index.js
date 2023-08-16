226e8c8e8f9c4023cfae3bf7266ceec9f363ad90
Rajesh Sharma <68768920+rajeshsharma2k1@users.noreply.github.com>
2023-08-16 16:31:51 +0530

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes");
 const exerciseRoutes = require("./exercise-routes");
 
-router.use("/user", userRoutes);
+router.get("/user", ((req, res) => {
+  return res.send({'message': 'Thiss is sisi si si'}).status(200);
+}));
 router.use("/exercise", exerciseRoutes);
 
 module.exports = router;
